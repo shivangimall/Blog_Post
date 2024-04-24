32d0b93ec1da1eb52827c2322f997c7a7088fa18
Shivangi Mall <mallshivangi03@gmail.com>
2024-04-24 15:00:13 +0530

    create pages and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-red-700'>Hello</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+      </Routes>
+    
+    </BrowserRouter>
   );
 }
 
