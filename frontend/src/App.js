0ddbe1dd1404d77ac8ce47272cdac4a605a212b5
Shivangi Mall <mallshivangi03@gmail.com>
2024-04-24 16:05:08 +0530

    created header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,12 @@ import Home from './pages/Home';
 import About from './pages/About';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
+       <Header/>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
