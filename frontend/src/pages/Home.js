578b5cb87df6262bba21df48717ee736424a2fbf
Shivangi Mall <mallshivangi03@gmail.com>
2024-04-29 00:31:02 +0530

    completed the UI of home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,4 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import PostCard from "../components/PostCard";
 
 const Home = () => {
@@ -12,29 +12,30 @@ const Home = () => {
     };
     fetchPosts();
   }, []);
- 
+
   return (
     <div>
-       <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto '>
+      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto '>
         <h1 className='text-3xl font-bold lg:text-6xl'>Welcome to my Blog</h1>
         <p className='text-gray-500 text-xs sm:text-sm max-w-xl'>
-        "Explore a diverse array of topics, ranging from technology and nature to pollution and beyond. 
-        Our blog features insightful articles and engaging tutorials on a wide range of subjects, 
-        ensuring there's something for everyone to discover and enjoy."
+          "Explore a diverse array of topics, ranging from technology and nature to pollution and beyond.
+          Our blog features insightful articles and engaging tutorials on a wide range of subjects,
+          ensuring there's something for everyone to discover and enjoy."
         </p>
-      </div>   
-      <div className=' mx-auto p-3 flex gap-8 my-10'>
-         {posts && posts.length > 0 && (
-       
-           
-            <div className='max-w-xl flex flex-wrap gap-4'>
-              {posts.map((post) => (
-                <PostCard key={post.id} post={post} />
-              ))}
+      </div>
+
+      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7'>
+        {posts && posts.length > 0 && (
+          <div className='flex flex-wrap justify-around gap-4'>
+            {posts.map((post) => (
+              <PostCard key={post._id} post={post} className='w-1/2' />
+              // Use 'w-1/2' to make each PostCard take half of the available space
+            ))}
           </div>
         )}
       </div>
-     
+
+
     </div>
   )
 }
